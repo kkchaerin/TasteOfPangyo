a8a4440b269cc9997d81e589cfe7c5dbb99d1e92
chaerin <krin0525@gmail.com>
2022-09-15 19:40:30 +0900

    backend 환경설정

---
+++ src/main/java/com/example/tasteofpangyoapi/TasteOfPangyoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasteofpangyoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteOfPangyoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteOfPangyoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tasteofpangyoapi/TasteOfPangyoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasteofpangyoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteOfPangyoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
